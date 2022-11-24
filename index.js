89d2fadde67c478eda6b368d116a5fbd75b145ad
unknown <foysalikram72@gmail.comm>
2022-11-25 00:13:24 +0600

    done creating api for all users

--- index.js
+++ index.js
@@ -19,3 +19,58 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => console.log(port))
 
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.send(401).send({ message: 'unauthorized ' })
+
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.Token, function (err, decoded) {
+        if (err) {
+            return res.status(401).send({ message: 'unauthorized access' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+
+ 
+
+}
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tngy8ld.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+
+    const userCollection =  client.db('Reseller').collection('users');
+
+    try{
+      
+        app.post('/users' , async (req,res)=>{
+            const users = req.body ;
+            const email = users.email ;
+            console.log(users)
+            const query = {email :email}
+            const search = await userCollection.find(query).toArray() ;
+            console.log(search)
+            if(search.length){
+                return 
+            }
+            const result =await userCollection.insertOne(users) ;
+           //console.log(search)
+            res.send(result)
+        })
+        app.get('/users', async (req,res)=>{
+            const query = {} ;
+            const users = await userCollection.find(query).toArray();
+            console.log(users)
+            res.send(users)
+        } )
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
